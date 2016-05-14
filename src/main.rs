2ffb89eab95c52e703c000c442a446ffe118ace0
James Hannah <jhannah8124@gmail.com>
2016-05-13 21:01:40 -0700

    added multiprocessed programming

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,97 @@
-use std::env;
-use std::fs::File;
-use std::io::{Read, Write, Error};
-use decrypt::decrypt;
+extern crate nix;
 
 mod decrypt;
 
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::error::Error;
+
+use decrypt::decrypt;
+
+use nix::unistd;
+
 fn main() {
     let mut args = env::args();
-    let input = args.nth(1);
-    let output = args.next();
-    if input.is_some() && output.is_some() {
-        match begin_decryption(input.unwrap(), output.unwrap()) {
+    let config = args.nth(1);
+    if config.is_some() {
+        match begin_decryption(config.unwrap()) {
             Ok(..) => {},
             Err(f) => println!("Error: {}", f),
         };
     }
 }
 
-fn begin_decryption(input: String, output: String) -> Result<(), Error>{
-    let mut input_file = try!(File::open(input));
-    let mut output_file = try!(File::create(output));
+fn begin_decryption(config: String) -> Result<(), String>{
+    let mut config_file = match File::open(config).map_err(modify_err) {
+        Ok(f) => f,
+        Err(e) => return Err(String::from(e))
+    };
     let mut st = String::new();
-    match input_file.read_to_string(&mut st) {
+    match config_file.read_to_string(&mut st) {
         Ok(..) => {
             for line in st.lines() {
-                match decrypt(line) {
-                    Ok(out) => try!(write!(output_file, "{}\n", out)),
-                    Err(1) => println!("Error: Incorrect length"),
-                    Err(2) => println!("Error: Unknown character"),
-                    Err(..) => println!("Error: Unknown error"),
-                }
+                let mut files = line.split_whitespace();
+                let in_file = files.next().unwrap();
+                let out_file = files.next().unwrap();
+                match unistd::fork().map_err(modify_err) {
+                    Ok(procc) => {
+                        if procc.is_child() {
+                            match child_work(in_file, out_file) {
+                                Ok(..) => println!("{} decrypted succesfully", in_file),
+                                Err(f) => println!("{} failed to decrypt: {}", in_file, f)
+                            };
+                        }
+                    },
+                    Err(e) => {
+                        return Err(e)
+                    }
+                };
             }
         },
         Err(..) => println!("Error reading file"),
     }
     Ok(())
+}
+
+fn child_work(in_file: &str, out_file: &str) -> Result<usize, String> {
+    let mut input = match File::open(in_file).map_err(modify_err) {
+        Ok(f) => f,
+        Err(e) => return Err(e)
+    };
+    let mut output = match File::create(out_file).map_err(modify_err) {
+        Ok(f) => f,
+        Err(e) => return Err(e)
+    };
+    let mut buf = String::new();
+    let _ = match input.read_to_string(&mut buf).map_err(modify_err) {
+        Ok(..) => {},
+        Err(e) => return Err(e)
+    };
+    let mut count = 0;
+
+    for line in buf.lines() {
+        match decrypt(line) {
+            Ok(out) => {
+                match write!(output, "{}\n", out).map_err(modify_err) {
+                    Ok(..) => count += 1,
+                    Err(e) => return Err(e)
+                };
+            },
+            Err(1) => println!("Error in {}: Incorrect Length", in_file),
+            Err(2) => println!("Error in {}: Unknown Character", in_file),
+            Err(..) => println!("Error in {}: Unknown Error", in_file)
+        };
+    }
+
+    if count < buf.lines().count() {
+        Err(String::from("Some lines not written"))
+    } else {
+        Ok(count)
+    }
+}
+
+fn modify_err<E: Error>(e: E) -> String {
+    let st = e.description();
+    String::from(st)
 }
